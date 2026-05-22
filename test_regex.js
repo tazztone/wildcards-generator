c500b0cae504cff9098b31e55cff429dc6f5f445
tazztone <62671577+tazztone@users.noreply.github.com>
2026-05-22 12:58:14 +0200

    🧹 Cleanup: Remove temporary debug file test_regex.js

--- test_regex.js
+++
@@ -1,9 +0,0 @@
-const str = "This is a test __my_var__ and ~~another_var~~ and __normal__";
-const regex1 = /(?:~~([^~]+)~~|__([^_]+)__)/g;
-const regex2 = /(?:~~(.+?)~~|__(.+?)__)/g;
-
-console.log("Original regex:");
-[...str.matchAll(regex1)].forEach(match => console.log(match));
-
-console.log("\nNew regex:");
-[...str.matchAll(regex2)].forEach(match => console.log(match));
